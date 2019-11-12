72ffe569ae368fd2279aa18a97906e5f20a76de0
Alexander Fukui <alexander.fukui@gmail.com>
2019-11-12 11:11:52 -0600

    3.1 done

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -3,3 +3,44 @@
 
 //2. Remove root
 // './Assets/RemoveRoot_Number.jpeg'
+
+//3. Create a BST class
+
+class BinarySearchTree {
+  constructor(key = null, value = null, parent = null) {
+    this.value = value,
+      this.key = key,
+      this.parent = parent,
+      this.left = null,
+      this.right = null
+  }
+
+  insert(key, value) {
+    if (this.key === null) {
+      this.key = key;
+      this.value = value;
+    }
+    else if (this.key >= key) {
+      if (this.left == null) {
+        this.left = new BinarySearchTree(key, value, this);
+      } else {
+        this.left.insert(key, value)
+      }
+    }
+    else {
+      if (this.right == null) {
+        this.right = new BinarySearchTree(key, value, this)
+      } else {
+        this.right.insert(key, value)
+      }
+    }
+  }
+
+  remove() {
+
+  }
+
+  find() {
+
+  }
+}
